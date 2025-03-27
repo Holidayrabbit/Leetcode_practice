cf20882697b6b7180f209727dfdf31a865f5fc31
Holidayrabbit <154126756@qq.com>
2025-03-27 12:45:03 +0800

    189

---
+++ 189.轮转数组.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=189 lang=python3
+#
+# [189] 轮转数组
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k%n
+        for i in range(k):
+            temp = nums[n-1]
+            nums.pop()
+            nums.insert(0,temp)
+
+        
+# @lc code=end
+
